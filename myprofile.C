dc88d232853fc4e79c95ad54fa8e48603ead5830
satishsinh32 <satishsinhvihol22@gmail.com>
2024-07-25 20:33:23 -0700

    Add files via upload

---
+++ myprofile.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf(("welcome to my profile\n"));
+	printf(("my name is satishsinh vihol\ni am 17 year old\none of my hobby is do somthing interesting always\nmy favorite game is kabbadi\n"));
+	printf(("i am from mirjhapur\nvadnagar\nmahesana\ngujrat\nidia,384355\n"));
+	printf(("my neighbor name is shouryapratapsinh\n"));
+	printf(("my coolege name is shanti bussiness school\ni am studing b.voc it in first year"));
+}
